6d1171b228be4ecb27f73a966b59ebfa975a3583
Sean Purcell <scppurce@edu.uwaterloo.ca>
2020-01-06 23:29:17 -0500

    Minor fix ups for clock
    Timings tested using periodic mode and an arm program that printed out
    the current timer value.

--- src/devices/timer.rs
+++ src/devices/timer.rs
@@ -29,6 +29,7 @@ pub struct Timer {
     // implementation details
     wrapmask: u32, // 0x0000FFFF for 16 bit timers, 0xFFFFFFFF for 32 bit timers
     last_time: Instant,
+    microticks: u32,
 }
 
 impl std::fmt::Debug for Timer {
@@ -49,14 +50,16 @@ impl Timer {
             clksel: Clock::Khz2,
             wrapmask: ((1u64 << bits) - 1) as u32,
             last_time: Instant::now(),
+            microticks: 0,
         }
     }
 
     /// Lazily update the registers on read / write.
     fn update_regs(&mut self) {
         // calculate the time delta
-        let dt = self.last_time.elapsed().as_nanos();
-        self.last_time = Instant::now();
+        let now = Instant::now();
+        let dt = now.duration_since(self.last_time).as_nanos() as u64;
+        self.last_time = now;
 
         if !self.enabled {
             return;
@@ -68,13 +71,15 @@ impl Timer {
         };
 
         // calculate number of ticks the timer should decrement by
-        let ticks = (khz as f64 * 0.000001) * dt as f64;
+        let microticks = dt * khz + self.microticks as u64;
         // FIXME: rounding down probably ain't the ideal behavior...
+        let ticks = (microticks / 1_000_000) as u32;
+        self.microticks = (microticks % 1_000_000) as u32;
         let ticks = ticks as u32;
 
         match self.mode {
             Mode::FreeRunning => {
-                self.val = self.val.wrapping_sub(ticks);
+                self.val = self.val.wrapping_sub(ticks) & self.wrapmask;
             }
             // XXX: double check this code...
             Mode::Periodic => {
@@ -127,8 +132,6 @@ impl Memory for Timer {
     fn w32(&mut self, offset: u32, val: u32) -> MemResult<()> {
         self.update_regs();
 
-        let val = val & self.wrapmask;
-
         match offset {
             0x00 => {
                 // "The Load register should not be written after the Timer is enabled because
@@ -138,6 +141,7 @@ impl Memory for Timer {
                     panic!("tried to write to {} Load while the timer is enabled");
                 }
 
+                let val = val & self.wrapmask;
                 self.loadval = Some(val);
                 // "The Timer Value register is updated with the Timer Load value as soon as the
                 // Timer Load register is written"
@@ -158,8 +162,12 @@ impl Memory for Timer {
                     true => Mode::Periodic,
                     false => Mode::FreeRunning,
                 };
+                let previous_enabled = self.enabled;
                 self.enabled = val & (1 << 7) != 0;
 
+                if self.enabled && !previous_enabled {
+                    self.microticks = 0;
+                }
                 if !self.enabled {
                     self.loadval = None;
                 }
