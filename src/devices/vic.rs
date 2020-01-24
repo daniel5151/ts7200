90497def942630da12a4781bce614759c78e5cfa
Sean Purcell <me@seanp.xyz>
2020-01-24 12:58:12 -0500

    Create VicManager struct and finish Vic definitions (#10)
    * Create VicManager struct and finish Vic definitions
    
    Tested only via software-requested interrupts for now.  Timer and UART
    interrupts coming in future PRs.
    
    * Review comments
    
    * Hardware identification values
    
    * nit

--- src/devices/vic.rs
+++ src/devices/vic.rs
@@ -1,23 +1,100 @@
+#![allow(clippy::unit_arg)] // Substantially reduces boilerplate
+
 use crate::memory::{MemResult, MemResultExt, Memory};
 
 /// VIC module
 ///
 /// As described in section 6
 /// https://www.student.cs.uwaterloo.ca/~cs452/F19/docs/ep93xx-user-guide.pdf
-pub struct Vic {
-    label: &'static str,
+
+#[derive(Debug)]
+struct VectorEntry {
+    source: u8,
+    isr_addr: u32,
+    enabled: bool,
 }
 
-impl std::fmt::Debug for Vic {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Vic").finish()
+impl Default for VectorEntry {
+    fn default() -> Self {
+        VectorEntry {
+            source: 0,
+            isr_addr: 0,
+            enabled: false,
+        }
     }
+}
+
+#[derive(Debug)]
+pub struct Vic {
+    label: &'static str,
+    status: u32,          // Interrupts currently hardware asserted
+    enabled: u32,         // Enabled interrupts
+    select: u32,          // FIQ mode interrupts
+    software_status: u32, // Software asserted interrupts
+
+    default_isr: u32,
+
+    vector_entries: [VectorEntry; 16],
 }
 
 impl Vic {
     /// Create a new Vic
     pub fn new(label: &'static str) -> Vic {
-        Vic { label }
+        Vic {
+            label,
+            status: 0,
+            enabled: 0,
+            select: 0,
+            software_status: 0,
+            default_isr: 0,
+            vector_entries: Default::default(),
+        }
+    }
+
+    fn rawstatus(&self) -> u32 {
+        self.software_status | self.status
+    }
+
+    fn enabled_active_interrupts(&self) -> u32 {
+        self.rawstatus() & self.enabled
+    }
+
+    /// Check if an IRQ should be requested
+    pub fn irq(&self) -> bool {
+        (self.enabled_active_interrupts() & !self.select) != 0
+    }
+
+    /// Check if an FIQ should be requested
+    pub fn fiq(&self) -> bool {
+        (self.enabled_active_interrupts() & self.select) != 0
+    }
+
+    fn isr_address(&self) -> u32 {
+        if self.fiq() || !self.fiq() {
+            self.default_isr
+        } else {
+            let irqs = self.enabled_active_interrupts() & !self.select;
+            self.vector_entries
+                .iter()
+                .find_map(|entry| {
+                    if entry.enabled && (irqs & (1 << entry.source)) != 0 {
+                        Some(entry.isr_addr)
+                    } else {
+                        None
+                    }
+                })
+                .unwrap_or(self.default_isr)
+        }
+    }
+
+    /// Request an interrupt from a hardware source
+    pub fn assert_interrupt(&mut self, source: u8) {
+        self.status |= 1 << source;
+    }
+
+    /// Clear an interrupt from a hardware source
+    pub fn clear_interrupt(&mut self, source: u8) {
+        self.status &= !(1 << source);
     }
 }
 
@@ -32,19 +109,59 @@ impl Memory for Vic {
 
     fn r32(&mut self, offset: u32) -> MemResult<u32> {
         match offset {
-            0x00 => crate::mem_unimpl!("STATUS_REG"),
-            0x10 => crate::mem_stub!("ENABLE_REG", 0),
-            0x14 => crate::mem_unimpl!("CLEAR_REG"),
+            0x00 => Ok(self.rawstatus() & !self.select),
+            0x04 => Ok(self.rawstatus() & self.select),
+            0x08 => Ok(self.rawstatus()),
+            0x0c => Ok(self.select),
+            0x10 => Ok(self.enabled),
+            0x14 => crate::mem_unexpected!(),
+            0x18 => Ok(self.software_status),
+            0x1c => Ok(0),
+            0x20 => crate::mem_stub!("PROTECTION", 0),
+            0x30 => Ok(self.isr_address()),
+            0x34 => Ok(self.default_isr),
+            0x100..=0x13c => {
+                let index = ((offset - 0x100) / 4) as usize;
+                Ok(self.vector_entries[index].isr_addr)
+            }
+            0x200..=0x23c => {
+                let index = ((offset - 0x200) / 4) as usize;
+                let entry = &self.vector_entries[index];
+                let result = (if entry.enabled { 0x20 } else { 0 }) + entry.source as u32;
+                Ok(result)
+            }
+            // Next 4 values are hardware identification values:
+            0xfe0 => Ok(0x90),
+            0xfe4 => Ok(0x11),
+            0xfe8 => Ok(0x04),
+            0xfec => Ok(0x00),
             _ => crate::mem_unexpected!(),
         }
         .mem_ctx(offset, self)
     }
 
-    fn w32(&mut self, offset: u32, _val: u32) -> MemResult<()> {
+    fn w32(&mut self, offset: u32, val: u32) -> MemResult<()> {
         match offset {
-            0x00 => crate::mem_unimpl!("STATUS_REG"),
-            0x10 => crate::mem_stub!("ENABLE_REG"),
-            0x14 => crate::mem_stub!("CLEAR_REG"),
+            0x0c => Ok(self.select = val),
+            0x10 => Ok(self.enabled = val),
+            0x14 => Ok(self.enabled &= !val),
+            0x18 => Ok(self.software_status |= val),
+            0x1c => Ok(self.software_status &= !val),
+            0x20 => crate::mem_stub!("PROTECTION"),
+            0x30 => crate::mem_stub!("VECTADDR"),
+            0x34 => Ok(self.default_isr = val),
+            0x100..=0x13c => {
+                let index = ((offset - 0x100) / 4) as usize;
+                Ok(self.vector_entries[index].isr_addr = val)
+            }
+            0x200..=0x23c => {
+                let index = ((offset - 0x200) / 4) as usize;
+                let entry = &mut self.vector_entries[index];
+                entry.enabled = (val & 0x20) != 0;
+                entry.source = (val & 0x1f) as u8;
+
+                Ok(())
+            }
             _ => crate::mem_unexpected!(),
         }
         .mem_ctx(offset, self)
---
+++ src/devices/vicmanager.rs
@@ -0,0 +1,140 @@
+use crate::memory::{MemResult, Memory};
+
+use super::Vic;
+
+/// VIC Manager module
+///
+/// Contains the two VIC units from the EP9302 and handles the daisy chaining
+/// logic.
+///
+/// As described in section 6
+/// https://www.student.cs.uwaterloo.ca/~cs452/F19/docs/ep93xx-user-guide.pdf
+
+// FIXME for prilik: unclear if this is the best place for this
+#[derive(Debug)]
+pub enum Interrupts {
+    Tc1Ui,
+    Tc2Ui,
+    Uart1RxIntr1,
+    Uart1TxIntr1,
+    Uart2RxIntr2,
+    Uart2TxIntr2,
+    Uart3RxIntr3,
+    Uart3TxIntr3,
+    Tc3Ui,
+    IntUart1,
+    IntUart2,
+    IntUart3,
+}
+
+impl Interrupts {
+    fn overall_index(&self) -> u8 {
+        use Interrupts::*;
+        match self {
+            Tc1Ui => 4,
+            Tc2Ui => 5,
+            Uart1RxIntr1 => 23,
+            Uart1TxIntr1 => 24,
+            Uart2RxIntr2 => 25,
+            Uart2TxIntr2 => 26,
+            Uart3RxIntr3 => 27,
+            Uart3TxIntr3 => 28,
+            Tc3Ui => 51,
+            IntUart1 => 52,
+            IntUart2 => 54,
+            IntUart3 => 55,
+        }
+    }
+    fn bank(&self) -> u8 {
+        if self.overall_index() < 32 {
+            1
+        } else {
+            2
+        }
+    }
+
+    fn index(&self) -> u8 {
+        self.overall_index() & !0x20
+    }
+}
+
+#[derive(Debug)]
+pub struct VicManager {
+    vic1: Vic,
+    vic2: Vic,
+}
+
+impl VicManager {
+    /// Create a new VicManager
+    #[allow(clippy::new_without_default)] // Prefer to force explicit creation
+    pub fn new() -> Self {
+        VicManager {
+            vic1: Vic::new("vic1"),
+            vic2: Vic::new("vic2"),
+        }
+    }
+
+    /// Check if an IRQ should be requested
+    pub fn fiq(&self) -> bool {
+        self.vic1.fiq() || self.vic2.fiq()
+    }
+
+    /// Check if an FIQ should be requested
+    pub fn irq(&self) -> bool {
+        self.vic1.irq() || self.vic2.irq()
+    }
+
+    fn bank(&mut self, bank: u8) -> &mut Vic {
+        match bank {
+            1 => &mut self.vic1,
+            2 => &mut self.vic2,
+            _ => panic!("Unexpected VIC bank {}", bank),
+        }
+    }
+
+    /// Request an interrupt from a hardware source
+    pub fn assert_interrupt(&mut self, int: Interrupts) {
+        self.bank(int.bank()).assert_interrupt(int.index())
+    }
+
+    /// Clear an interrupt from a hardware source
+    pub fn clear_interrupt(&mut self, int: Interrupts) {
+        self.bank(int.bank()).clear_interrupt(int.index())
+    }
+}
+impl Memory for VicManager {
+    fn device(&self) -> &'static str {
+        "VicManager"
+    }
+
+    fn r32(&mut self, offset: u32) -> MemResult<u32> {
+        match offset {
+            0x30 => {
+                // Daisy chain the VICVectAddr register
+                if self.vic1.irq() {
+                    self.vic1.r32(0x30)
+                } else if self.vic2.irq() {
+                    self.vic2.r32(0x30)
+                } else {
+                    // TODO: Result in this case unclear, needs hardware checking
+                    self.vic1.r32(0x34) // Read the VIC1DefVectAddr register
+                }
+            }
+            _ => {
+                if offset < 0x10000 {
+                    self.vic1.r32(offset)
+                } else {
+                    self.vic2.r32(offset - 0x10000)
+                }
+            }
+        }
+    }
+
+    fn w32(&mut self, offset: u32, val: u32) -> MemResult<()> {
+        if offset < 0x10000 {
+            self.vic1.w32(offset, val)
+        } else {
+            self.vic2.w32(offset - 0x10000, val)
+        }
+    }
+}
