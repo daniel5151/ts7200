caa1b60de372c03f55a568e35beb7ac80a519a88
Daniel Prilik <danielprilik@gmail.com>
2020-02-10 16:37:48 -0500

    more syscon functionality + hardware validation

--- src/devices/syscon.rs
+++ src/devices/syscon.rs
@@ -1,18 +1,26 @@
+use log::*;
+
 use crate::memory::{MemResult, MemResultExt, Memory};
 
 /// System Controller module
 ///
-/// As described in section 5 of the EP93xx User's Guide
+/// As described in section 5 of the EP93xx User's Guide.
 #[derive(Debug)]
 pub struct Syscon {
+    scratch_reg: [u32; 2],
     device_cfg: u32,
+    is_locked: bool,
 }
 
 impl Syscon {
     /// Create a new System Controller
-    // XXX: hardware validate Syscon default values
     pub fn new_hle() -> Syscon {
-        Syscon { device_cfg: 0 }
+        Syscon {
+            scratch_reg: [0, 0],
+            // Enabled Bits: GonK CPENA U2EN U1EN HonIDE GonIDE EonIDE
+            device_cfg: 0x0894_0d00, // hardware validated
+            is_locked: true,
+        }
     }
 }
 
@@ -53,15 +61,32 @@ impl Memory for Syscon {
         match offset {
             0x00 => crate::mem_unimpl!("PwrSts"),
             0x04 => crate::mem_unimpl!("PwrCnt"),
-            0x08 => crate::mem_unimpl!("Halt"),
-            // TODO: actually enter "low power mode" somehow
-            0x0C => crate::mem_stub!("Standby"),
+            0x08 => {
+                if self.device_cfg & 1 == 1 {
+                    // TODO: actually enter "halt" mode somehow
+                    warn!("Entered Halt mode");
+                    Ok(0) // doesn't matter
+                } else {
+                    // FIXME: emit warning when device contract is violated (instead of panic)
+                    panic!("Cannot enter Halt mode if SHena != 1 in syscon DeviceCfg");
+                }
+            }
+            0x0C => {
+                if self.device_cfg & 1 == 1 {
+                    // TODO: actually enter "low power mode" somehow
+                    warn!("Entered Standby mode");
+                    Ok(0) // doesn't matter
+                } else {
+                    // FIXME: emit warning when device contract is violated (instead of panic)
+                    panic!("Cannot enter Standby mode if SHena != 1 in syscon DeviceCfg");
+                }
+            }
             0x18 => crate::mem_unimpl!("TEOI"),
             0x1C => crate::mem_unimpl!("STFClr"),
             0x20 => crate::mem_unimpl!("ClkSet1"),
             0x24 => crate::mem_unimpl!("ClkSet2"),
-            0x40 => crate::mem_unimpl!("ScratchReg0"),
-            0x44 => crate::mem_unimpl!("ScratchReg1"),
+            0x40 => Ok(self.scratch_reg[0]),
+            0x44 => Ok(self.scratch_reg[1]),
             0x50 => crate::mem_unimpl!("APBWait"),
             0x54 => crate::mem_unimpl!("BusMstrArb"),
             0x58 => crate::mem_unimpl!("BootModeClr"),
@@ -72,27 +97,49 @@ impl Memory for Syscon {
             0x90 => crate::mem_unimpl!("KeyTchClkDiv"),
             0x94 => crate::mem_unimpl!("ChipID"),
             0x9C => crate::mem_unimpl!("SysCfg"),
-            0xC0 => crate::mem_unimpl!("SysSWLock"),
+            0xC0 => {
+                if self.is_locked {
+                    Ok(0x00)
+                } else {
+                    Ok(0x01)
+                }
+            }
             _ => crate::mem_unexpected!(),
         }
         .mem_ctx(offset, self)
     }
 
     fn w32(&mut self, offset: u32, val: u32) -> MemResult<()> {
+        if (0x80..=0x9C).contains(&offset) {
+            if self.is_locked {
+                // FIXME: emit warning when device contract is violated (instead of panic)
+                panic!(
+                    "Attempted to writing to SW locked syscon register (offset {:#x?})!",
+                    offset
+                );
+            } else {
+                // syscon re-locks after a locked register has been written to
+                self.is_locked = true;
+            }
+        }
+
         match offset {
             0x00 => crate::mem_unimpl!("PwrSts"),
             0x04 => crate::mem_unimpl!("PwrCnt"),
-            0x08 => crate::mem_unimpl!("Halt"),
+            0x08 => {
+                // XXX: don't panic if writing to a read-only register
+                panic!("tried to write value to read-only syscon Halt register!");
+            }
             0x0C => {
                 // XXX: don't panic if writing to a read-only register
-                panic!("tried to write value to read-only syscon register!");
+                panic!("tried to write value to read-only syscon Standby register!");
             }
             0x18 => crate::mem_unimpl!("TEOI"),
             0x1C => crate::mem_unimpl!("STFClr"),
             0x20 => crate::mem_unimpl!("ClkSet1"),
             0x24 => crate::mem_unimpl!("ClkSet2"),
-            0x40 => crate::mem_unimpl!("ScratchReg0"),
-            0x44 => crate::mem_unimpl!("ScratchReg1"),
+            0x40 => Ok(self.scratch_reg[0] = val),
+            0x44 => Ok(self.scratch_reg[1] = val),
             0x50 => crate::mem_unimpl!("APBWait"),
             0x54 => crate::mem_unimpl!("BusMstrArb"),
             0x58 => crate::mem_unimpl!("BootModeClr"),
@@ -103,7 +150,15 @@ impl Memory for Syscon {
             0x90 => crate::mem_unimpl!("KeyTchClkDiv"),
             0x94 => crate::mem_unimpl!("ChipID"),
             0x9C => crate::mem_unimpl!("SysCfg"),
-            0xC0 => crate::mem_unimpl!("SysSWLock"),
+            0xC0 => {
+                if val == 0xAA {
+                    self.is_locked = false;
+                } else {
+                    // FIXME: emit warning when device contract is violated (instead of panic)
+                    panic!("wrote non-0xAA value to SysSWLock register!");
+                }
+                Ok(())
+            }
             _ => crate::mem_unexpected!(),
         }
         .mem_ctx(offset, self)
