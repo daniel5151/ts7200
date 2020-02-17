9cd7fcf4c13c6130f847a94596dd633cc64940ee
Daniel Prilik <danielprilik@gmail.com>
2020-02-17 14:12:44 -0500

    _properly_ fix uninitialized RAM warnings
    turns out the bug _was_ in our C++ code.

--- src/devices/ram.rs
+++ src/devices/ram.rs
@@ -2,7 +2,10 @@ use std::vec::Vec;
 
 use byteorder::{ByteOrder, LittleEndian};
 
-use crate::memory::{MemException::*, MemResult, Memory};
+use crate::memory::{
+    MemException::{self, *},
+    MemResult, Memory,
+};
 
 /// Basic fixed-size RAM module.
 pub struct Ram {
@@ -38,19 +41,32 @@ impl Ram {
             .for_each(|b| *b = true);
     }
 
-    fn addr_as_str(&self, offset: usize, size: usize) -> String {
-        let s = self.initialized[offset..offset + size]
+    fn uninit_read(&self, offset: usize, size: usize, stub: u32) -> MemException {
+        let mut partially_init = false;
+        let data = self.initialized[offset..offset + size]
             .iter()
             .zip(self.mem[offset..offset + size].iter())
             .map(|(init, val)| {
                 if *init {
+                    partially_init = true;
                     format!("{:02x?}", val)
                 } else {
                     "??".to_string()
                 }
             })
             .collect::<String>();
-        format!("0x{}", s)
+
+        let msg = if partially_init {
+            format!("r{} from partially uninitialized RAM: 0x{}", size * 8, data)
+        } else {
+            format!("r{} from uninitialized RAM", size * 8,)
+        };
+
+        ContractViolation {
+            msg,
+            severity: log::Level::Warn,
+            stub_val: Some(stub as u32),
+        }
     }
 }
 
@@ -66,16 +82,8 @@ impl Memory for Ram {
     fn r8(&mut self, offset: u32) -> MemResult<u8> {
         let offset = offset as usize;
         let val = self.mem[offset];
-
         if !self.initialized[offset] {
-            return Err(ContractViolation {
-                msg: format!(
-                    "r8 from (partially) uninitialized RAM: {}",
-                    self.addr_as_str(offset, 1)
-                ),
-                severity: log::Level::Warn,
-                stub_val: Some(val as u32),
-            });
+            return Err(self.uninit_read(offset, 1, val as u32));
         }
         Ok(val)
     }
@@ -84,14 +92,7 @@ impl Memory for Ram {
         let offset = offset as usize;
         let val = LittleEndian::read_u16(&self.mem[offset..offset + 2]);
         if self.initialized[offset..offset + 2] != [true; 2] {
-            return Err(ContractViolation {
-                msg: format!(
-                    "r16 from (partially) uninitialized RAM: {}",
-                    self.addr_as_str(offset, 2)
-                ),
-                severity: log::Level::Warn,
-                stub_val: Some(val as u32),
-            });
+            return Err(self.uninit_read(offset, 2, val as u32));
         }
         Ok(val)
     }
@@ -100,44 +101,28 @@ impl Memory for Ram {
         let offset = offset as usize;
         let val = LittleEndian::read_u32(&self.mem[offset..offset + 4]);
         if self.initialized[offset..offset + 4] != [true; 4] {
-            return Err(ContractViolation {
-                msg: format!(
-                    "r32 from (partially) uninitialized RAM: {}",
-                    self.addr_as_str(offset, 4)
-                ),
-                severity: log::Level::Warn,
-                stub_val: Some(val as u32),
-            });
+            // gcc likes to emit 8-bit store instructions, but later read those values via
+            // 32 bit read instructions. To squelch these errors, word-aligned reads are
+            // allowed to return partially uninitialized words.
+            if self.initialized[offset & !0x3] {
+                return Ok(val);
+            } else {
+                return Err(self.uninit_read(offset, 4, val));
+            }
         }
         Ok(val)
     }
 
     fn w8(&mut self, offset: u32, val: u8) -> MemResult<()> {
         let offset = offset as usize;
-
-        // gcc likes to emit 8-bit store instructions, but then read the value via 32
-        // bit read instructions. To squelch these errors, word-aligned writes are
-        // treated as through they've initialized the entire word.
-        if offset & 0x3 == 0 {
-            self.initialized[offset..offset + 4].copy_from_slice(&[true; 4]);
-        } else {
-            self.initialized[offset] = true;
-        }
-
+        self.initialized[offset] = true;
         self.mem[offset] = val;
         Ok(())
     }
 
     fn w16(&mut self, offset: u32, val: u16) -> MemResult<()> {
         let offset = offset as usize;
-
-        // see comment in w8
-        if offset & 0x3 == 0 {
-            self.initialized[offset..offset + 4].copy_from_slice(&[true; 4]);
-        } else {
-            self.initialized[offset..offset + 2].copy_from_slice(&[true; 2]);
-        }
-
+        self.initialized[offset..offset + 2].copy_from_slice(&[true; 2]);
         LittleEndian::write_u16(&mut self.mem[offset..offset + 2], val);
         Ok(())
     }
